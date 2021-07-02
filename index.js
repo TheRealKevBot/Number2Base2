dc7d5d448b40867fc1beb65bf910d4fba7175ad0
TheRealKevBot <Kglas001@gmail.com>
2021-07-01 18:31:36 -0600

    Build

--- index.js
+++ index.js
@@ -5,9 +5,15 @@
 // every bit to the left will be x2 the value, value of an 8 bit binary 
 // numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
 
+
+// https://edabit.com/challenge/3kcrnpHk7krNZdnKK
+
+
 function binary(decimal) {
-	
+    let answer = decimal.toString(2) // The number will show as a binary string value
+    return answer
 } 
+
 
 console.log(binary(100), "1100100")
 console.log(binary(1), "1")
